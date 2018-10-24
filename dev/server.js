988e876bc62b771445339d9629305bbd1d04dc05
Arpit Jain <antimatter96@users.noreply.github.com>
2018-10-25 00:54:46 +0530

    dev server update

--- dev/server.js
+++ dev/server.js
@@ -1,17 +1,21 @@
-var entries = require('./entries.js');
+var fs = require("fs");
+var path = require("path");
+let dataString = fs.readFileSync(path.join(__dirname, "../src/data.json"));
+var entries = JSON.parse(dataString);
+var toSend = {"data":entries};
 
 var express = require('express');
 
 var app = express();
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
 	req.accepts('application/json');
 	res.set('Access-Control-Allow-Origin', "*");
-	res.send(entries);
+	res.send(toSend);
 });
 
 app.listen(8080, function() {
 	console.log("Running on port ", 8080);
 });
 
-console.log(entries.data.length);
+console.log(entries.length);
--- src/render.js
+++ src/render.js
@@ -1,7 +1,7 @@
 var renderer = (function() {
-	
+
 	var isMobile = false;
-	
+
 	function render(filteredData) {
 		const startTime = performance.now();
 		let deleteThis = document.getElementById("list");
@@ -11,63 +11,62 @@ var renderer = (function() {
 		else {
 			console.log("First ?");
 		}
-	
+
 		let list = document.createElement("div");
 		list.id = "list";
 
 		for( let i = 0; i < filteredData.length; i++) {
 			let e = filteredData[i];
-			
+
 			let listEntry = document.createElement("div");
-			
+
 			let listDivContainer = document.createElement("div");
 			listEntry.classList.add("listentry-container");
 			listDivContainer.classList.add("listEntry");
-			
+
 			let entryName = document.createElement("span");
 			entryName.innerText = e.name;
 			entryName.classList.add("entry-name");
-			
+
 			listDivContainer.appendChild(entryName);
-			
+
 			let entryLink = document.createElement("a");
 			entryLink.href = e.url;
 			let entryLinkMain = document.createElement("span");
 			entryLinkMain.innerText = "Here";
 			entryLink.classList.add("entry-link");
-			
+
 			entryLink.appendChild(entryLinkMain);
 
-			
-			
+
+
 			if( e.locations ) {
-			
+
 				let locationDiv = document.createElement("div");
 				locationDiv.classList.add("location-container");
-			
+
 				for(let x = 0; x < e.locations.length; x++) {
 					let location = document.createElement("span");
 					location.classList.add("location-entry");
-					
+
 					let locationName = document.createElement("span");
 					let locationCountry = document.createElement("span");
-					
+
 					locationName.innerText = e.locations[x].loc;
 					locationCountry.innerText = e.locations[x].country;
-					
+
 					locationName.classList.add("location-name");
 					locationCountry.classList.add("location-country");
-					
+
 					location.appendChild(locationName);
 					location.appendChild(locationCountry);
-					
+
 					locationDiv.appendChild(location);
 				}
-				
-				
+
 				listDivContainer.appendChild(locationDiv);
 			}
-			
+
 			if(e.remote) {
 				let remote = document.createElement("div");
 				remote.innerText = "Remote : Yes";
@@ -75,28 +74,28 @@ var renderer = (function() {
 				listDivContainer.appendChild(remote);
 
 			}
-			
+
 			listDivContainer.appendChild(entryLink);
-			
+
 			listEntry.appendChild(listDivContainer);
 			list.appendChild(listEntry);
 		}
 		document.body.appendChild(list);
 		const duration = performance.now() - startTime;
 		console.log("Rendering of", filteredData.length, "took", duration, "ms");
-		
+
 		if(!isMobile) {
 			const startTimeM = performance.now();
 			var ms = new Masonry("#list", {
 				horizontalOrder : true,
 				itemSelector: ".listentry-container"
 			});
-			
+
 			const durationM = performance.now() - startTimeM;
 			console.log("Masonry", filteredData.length, "took", durationM, "ms");
 		}
 	}
-	
+
 	return {
 		display:function(loc_data) {
 			if(navigator.userAgent) {
@@ -104,7 +103,7 @@ var renderer = (function() {
 					isMobile = true;
 				}
 			}
-			render(loc_data);	
+			render(loc_data);
 		}
 	};
 })();
